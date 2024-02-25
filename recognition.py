f9b79d5f222589d4c4ca392c4fcd2cec42e455dd
Yoyo <52893812+exanova-y@users.noreply.github.com>
2024-02-24 21:12:27 -0800

    added circularity ratio

--- recognition.py
+++ recognition.py
@@ -10,15 +10,29 @@ while True: # continuously capture frames from the webcam
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
 
 
-    # use a colour bound with as a heuristic
-    lower_orange = np.array([33, 48, 89]) # lowest HSV, respectively
-    upper_orange = np.array([24, 74, 65]) # highest HSV
+    # Right now, we use three heuristics for recognizing circles:
+    # 1: colour bound
+    # 2: circularity ratio of a circle
+    # 3: area > 500 pixels on screen
+    lower_orange = np.array([10, 130, 100]) # lowest HSV, respectively
+    upper_orange = np.array([25, 255, 255]) # highest HSV
     mask = cv2.inRange(hsv, lower_orange, upper_orange) 
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+
     for cnt in contours: # for all the identified counter
         area = cv2.contourArea(cnt)
-        if area > 100:  # Min area of 500 pixels to reduce noise
+        perimeter = cv2.arcLength(cnt, True)
+
+        # the orange is always a circle. We check for circularity
+        # area/perimeter = (pi*r**2)/(2*pi*r)**2 = 1/4pi ~= 0.079
+        # roughly, the ratio should be < 0.1
+        ratio = 1 # initialize ratio to 1
+        if perimeter != 0:
+            ratio = area/(perimeter**2)  
+        print(f"Area: {area}, Area-perimeter ratio: {ratio}")
+        
+        if area > 500 and (ratio > 0.055 and ratio < 0.075): 
             # (0, 0) is the top left corner of the image
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) # a bounding rectangle
