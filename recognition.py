b6df7bfa21cfda6f981d0c0f24d67eb61d6f99a0
Amina Rakhimbergenova <amoka_r@bk.ru>
2024-02-25 12:12:33 -0800

    final

--- recognition.py
+++ recognition.py
@@ -1,16 +1,187 @@
 import cv2 # the OpenCV library
 import numpy as np
+import socket
+import time
+import re
+class PA3400:
+    # Network interface with Precise Automation PF3400 SCARA arm
+    # Requires "Tcp_cmd_server" to be running on arm to accept TCP connections
+    # and accept Guidance Programming Language (GPL) commands
+
+    def __init__(self, address):
+        self.host = address['host']
+        self.port = address['port']
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Home position joint angles
+        self.homej = [750, 0, 180, -180]
+        self.curpos = []
+
+        # Maximum speed limit %
+        self.rapid = 20
+
+    def connect(self):
+        # Attempt to open TCP stream with robot
+        try:
+            self.sock.connect((self.host, self.port))
+            print('Success')
+        except socket.error as msg:
+            print('Socket Error: ', msg)
+
+    def enable(self):
+        # Enable high power, attach robot 1 (local addr)
+        self.sendcmd('mode 0')
+        self.sendcmd('hp 1')
+        time.sleep(4)
+        self.sendcmd('attach 1')
+        time.sleep(1)
+        self.sendcmd('home')
+        time.sleep(5)
+
+    def disable(self):
+        # Disable robot high power
+        self.sendcmd('hp 0')
+
+    def disconnect(self):
+        # Close TCP stream with robot
+        self.sock.close()
+
+    def movej(self, pos):
+        # Move robot to specified joint positions
+        # pos = [H, A1, A2, A3, A4]
+        # movej <profile#> <height> <angle2> <angle3> <angle4>
+        cmd = 'movej 1 ' + ' '.join(str(n) for n in pos)
+        self.sendcmd(cmd)
+
+    def movec(self, pos):
+        # Move robot to specified cartesian position, computing all joint
+        # inverse kinematics.
+        # pos = [X, Y, Z, Y, P, R]
+        #
+        # movec <profile#> <X> <Y> <Z> <Yaw> <pitch> <roll> [<handedness>]
+        self.curpos = pos
+        cmd = 'movec 1 ' + ' '.join(str(n) for n in pos)
+        self.sendcmd(cmd)
+
+    def movea(self, pos1, pos2):
+        # Move robot to specified cartesian position, computing all joint
+        # inverse kinematics.
+        # pos = [X, Y, Z, Y, P, R]
+        #
+        # movec <profile#> <X> <Y> <Z> <Yaw> <pitch> <roll> [<handedness>]
+        self.curpos = pos
+        cmd = 'movea 1 ' + ' '.join(str(n) for n in pos1) + ' '.join(str(n) for n in pos2)
+        self.sendcmd(cmd)
+
+    def gohome(self):
+        self.movej(self.homej)
+
+    def sendcmd(self, cmd):
+        # Attempt to send command on socket
+        #
+        # TODO add error handling
+        cmd += '\n'
+        self.sock.send(cmd.encode())
+        print('Send command: ' + cmd)
+        ack = self.sock.recv(1024)
+        print(ack.decode())
+
+    def parsegc(self, line):
+        # Attempt to convert gcode block to PA command
+        #
+
+        blocks = line.split()
+
+        if not len(blocks):
+            return None
+        # cmd = blocks[0]
+        #
+
+        # Extract letter coordinates
+        blockchars = ['G','M','F','X','Y','Z','I','J','K']
+        blockvals = dict.fromkeys(blockchars, 0)
+
+        for letter in blockvals:
+            block = re.search('(?<!\((?:(?!\))[\s\S\r])*?)' + letter + '-?\d*\.?\d*', line.upper())
+            if block:
+                if letter in ['G', 'M']:
+                    blockvals[letter] = int(block.group()[1:])
+                else:
+                    blockvals[letter] = float(block.group()[1:])
+
+        xyz = [blockvals['X'], blockvals['Y'], blockvals['Z']]
+        ijk = [blockvals['I'], blockvals['J'], blockvals['K']]
+        ypr = [90, -180, 1] # HACK
+        g = blockvals['G']
+
+        # bail if it's not a G command
+        if not re.search('(?<!\((?:(?!\))[\s\S\r])*?)' + 'G' + '-?\d*\.?\d*', line.upper()):
+            return None
+
+        if g is 0:
+            self.maxSpeed = 100
+            pos = xyz + ypr
+            self.movec(pos)
+
+        elif g is 1:
+            self.maxSpeed = 10
+            pos = xyz + ijk
+            self.movec(pos)
+
+        elif g is 2:
+            self.maxSpeed = 10
+
+            # Convert center point arc to 3 point arc
+            end = numpy.array(tuple(xyz))
+            cur = numpy.array(tuple(self.curpos[:3]))
+            cen = cur + numpy.array(tuple(ijk))
+            midchord = cur + ((end - cur ) / 2)
+            midarc = cen + ((midchord - cen) * numpy.linalg.norm(cur - cen) / numpy.linalg.norm(midchord - cen))
+
+            pos1 = midarc + ypr
+            pos2 = xyz + ypr
+            self.movea(pos1, pos2)
+
+
+        elif g is 3:
+            self.maxSpeed = 10
+            end = numpy.array(tuple(xyz))
+            cur = numpy.array(tuple(self.curpos[:3]))
+            cen = cur + numpy.array(tuple(ijk))
+            midchord = cur + ((end - cur ) / 2)
+            midarc = cen + ((midchord - cen) * numpy.linalg.norm(cur - cen) / numpy.linalg.norm(midchord - cen))
+            pos1 = midarc + ypr
+            pos2 = xyz + ypr
+            self.movea(pos1, pos2)
+
+        else:
+            return
+
+    @property
+    def maxSpeed(self):
+        return self.rapid
+
+    @maxSpeed.setter
+    def maxSpeed(self, speed):
+        self.sendcmd('mspeed ' + str(speed))
+        self.rapid = speed
+
+
+
+
 
 cap = cv2.VideoCapture(0)  # Use the first webcam. can experiment with 0, 1, 2 etc.
 
-robot = PA3400{'host': '192.168.0.168', 'port': 22}
+robot = PA3400({'host': '10.10.10.40', 'port':10100 })
 
 robot.connect()
 robot.enable()
 
-robot.maxSpeed = 30
-pos = [-200, -155, 244, 179.99, 90, -180, 2]
-robot.movec(pos)
+robot.maxSpeed = 100
+
+
+pos = [-200, 90, 0, 0, 0]
+robot.movej(pos)
 
 while True: # continuously capture frames from the webcam
     _, frame = cap.read() 
@@ -49,7 +220,7 @@ while True: # continuously capture frames from the webcam
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) # a bounding rectangle
             centroid_x = x + w//2
             centroid_y = y + h//2
-            print(f"Centroid: ({centroid_x}, {centroid_y})")  # Print coordinates
+            print("Centroid: ({centroid_x}, {centroid_y})")  # Print coordinates
 
         if 0 < centroid_x <= 200:
             print("1")
--- script.sh
+++
@@ -1,6 +0,0 @@
-sshpass -p 'automation' ssh gordon@192.168.0.168
-'
-cd spicebot/ &&
-cd robot_playground/ &&
-python main.py
-'
